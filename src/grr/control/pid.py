398bc1f8de8dd2787afe80635ec035668cc1a825
Philip Smith <philip@randomsmiths.com>
2023-06-20 00:36:16 -0400

    add plotting functionality

--- src/grr/control/pid.py
+++ src/grr/control/pid.py
@@ -1,5 +1,11 @@
 from time import perf_counter
 from grr.utilities.clamp_range import clamp_range
+import matplotlib
+import matplotlib.pyplot as plt
+
+from sys import platform as sys_pf
+if sys_pf == 'darwin':
+    matplotlib.use("TkAgg")
 
 
 class PID:
@@ -11,16 +17,29 @@ class PID:
     deltaT: float
     _integral: float = 0
     _previous_error: float = 0
-    _previous_time: float = 0
+    _previous_time: float = None
+    goal = 0
+    plot = False
+    xList = []
+    yList1 = []
+    yList2 = []
+    yList3 = []
+    yList4 = []
+    yList5 = []
 
-    def __init__(self, kP: float, kI: float, kD: float, min_clamp=float('-inf'), max_clamp=float('inf')) -> None:
+
+
+    def __init__(self, kP: float, kI: float, kD: float, min_clamp=float('-inf'), max_clamp=float('inf'), plot=False) -> None:
         self.kP = kP
         self.kI = kI
         self.kD = kD
         self.min_clamp = min_clamp
         self.max_clamp = max_clamp
-
+        self.plot = plot
+        if plot:
+            self.fig, (self.ax1, self.ax2, self.ax3, self.ax4, self.ax5) = plt.subplots(5, sharex=True)
     def calculate(self, goal: float, current: float) -> float:
+        self.goal = goal
         if not self._previous_time:
             self.deltaT = 0.01
             self._previous_time = perf_counter()
@@ -44,5 +63,36 @@ class PID:
         output = clamp_range(output, self.min_clamp, self.max_clamp)
 
         self._previous_error = error
+        
+        if self.plot:
+            self.xList.append(self._previous_time)
+            self.yList1.append(current)
+            self.yList2.append(error)
+            self.yList3.append(self._integral)
+            self.yList4.append(derivitive)
+            self.yList5.append(output)
+            self.ax1.cla()
+            self.ax1.plot(self.xList, self.yList1)
+            self.ax1.plot(self.xList, [goal]*len(self.yList1))
+            self.ax1.set(ylabel='value')
+            self.ax2.cla()
+            self.ax2.plot(self.xList, self.yList2)
+            self.ax2.set(ylabel='error')
+            self.ax3.cla()
+            self.ax3.plot(self.xList, self.yList3)
+            self.ax3.set(ylabel='integral')
+            self.ax4.cla()
+            self.ax4.plot(self.xList, self.yList4)
+            self.ax4.set(ylabel='derivitive')
+            self.ax5.cla()
+            self.ax5.plot(self.xList, self.yList5)
+            self.ax5.set(ylabel='output')
+            plt.pause(.00000000000000001)
 
         return output
+
+    def reset(self):
+        self._integral = 0
+        self._previous_error = 0
+        self._previous_time = None
+        self.goal = 0
--- tests/test_control.py
+++ tests/test_control.py
@@ -1,12 +1,56 @@
 import pytest
+import numpy as np
+import turtle
 
 from grr.control.pid import PID
+from time import sleep
 
 
-@pytest.mark.parametrize("kP, kI, kD, min_clamp, max_clamp, goal, values, expected", [
-    (1, 0, 0, -1, 1, 0, [2, 1.5, 1, .5, 0], [-1, -1, -1, -.5, 0])
+@pytest.mark.parametrize("kP, kI, kD, min_clamp, max_clamp, goal, values, expected, dT", [
+    (1, 0, 0, -1, 1, 0, [2, 1.5, 1, .5, 0], [-1, -1, -1, -.5, 0], .1),
+    (1, 0, 0, -1, 1, 0, [-2, -1.5, -1, -.5, 0], [1, 1, 1, .5, 0], .1),
+    (0, 1, 0, -1, 1, 0, [2, 1.5, 1, .5, 0], [-0.036, -0.19358575046493207, -0.2955632294978714, -0.3458556460172404, -0.3468338745078072], .1),
+    (0, 0, 1, -1, 1, 0, [2, 1.5, 1, .5, 0], [-1, 1, 1, 1, 1], .1),
+
 ])
-def test_PID(kP: float, kI: float, kD: float, min_clamp: float, max_clamp: float, goal: float, values: list[float], expected: list[float]):
+def test_PID_predetermined(kP: float, kI: float, kD: float, min_clamp: float, max_clamp: float,
+                           goal: float, values: list[float], expected: list[float], dT: float):
     pid = PID(kP, kI, kD, min_clamp, max_clamp)
     for i, val in enumerate(values):
-        assert pid.calculate(goal, val) == expected[i]
+        assert np.isclose(pid.calculate(goal, val), expected[i], .01, .01)
+        sleep(dT)
+
+
+@pytest.mark.parametrize("kP, kI, kD, goal, start", [
+    (1, 0, 0, 0, 10),
+    (1, 1, 0, 5.5, 20)
+])
+def test_convergence(kP, kI, kD, goal: float, start: float):
+    pid = PID(kP, kI, kD, -1, 1, True)
+    curr = start
+    while not np.isclose(curr, goal):
+        curr += pid.calculate(goal, curr)
+        print(curr)
+        sleep(.01)
+
+def simulator(kP, kI, kD, goal, start, plot):
+    pid = PID(kP, kI, kD, 0, 50, plot)
+    t = turtle.Turtle()
+    t.penup()
+    t.goto(-50, goal)
+    t.pendown()
+    t.forward(100)
+    t.penup()
+    t.goto(0, start)
+    t.left(90)
+    curr = start
+    dy = 0
+    while True:
+        ddy = -9.8 + pid.calculate(goal, curr)
+        dy += ddy * .1
+        curr = curr + dy * .1
+        t.goto(0, curr)
+        sleep(.1)
+
+if __name__ == "__main__":
+    simulator(.0, 0, 0, 150, -150, True)
